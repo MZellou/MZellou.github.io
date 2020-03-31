21e50a4740c37a44f15fcdc88d5be57b5b615b37
mzellou <marouane.zellou@developpement-durable.gouv.fr>
2020-03-31 10:23:55 +0200

    timeline intégration

---
+++ HTML/timeline.R
@@ -0,0 +1,65 @@
+'<div class="page">
+  <div class="timeline">
+    <div class="timeline__group">
+      <span class="timeline__year">2014</span>
+      <div class="timeline__box">
+        <div class="timeline__date">
+          <span class="timeline__day">1</span>
+          <span class="timeline__month">Sept</span>
+        </div>
+        <div class="timeline__post">
+          <div class="timeline__content">
+            <p>École Nationale des Travaux Publics de l\'État : blablabla</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="timeline__group">
+      <span class="timeline__year">2016</span>
+      <div class="timeline__box">
+        <div class="timeline__date">
+          <span class="timeline__day">1</span>
+          <span class="timeline__month">Sept</span>
+        </div>
+        <div class="timeline__post">
+          <div class="timeline__content">
+            <p><b>Projet spécifique de recherche au sein du Laboratoire d\'Ingéniérie Circulation Transport (LICIT): Lyon, France (Java): </b>
+            <li>Travail au sein d’une équipe de recherche dans le cadre du projet MOCoPo dirigé par le Dr Christine BUISSON.</li>
+            <li>Mise a jour du site internet du projet (Typo3).</li>
+            <li>Mise à jour du programme Java de traitement d\'images : Ce programme permet l\'analyse des conditions de circulation à partir de photos
+            aériennes. Pour plus d\'informations : <a href="https://mocopo.ifsttar.fr/">visitez le site de MOCoPo</a>  </li>
+          </div>
+        </div>
+      </div>
+      <div class="timeline__box">
+        <div class="timeline__date">
+          <span class="timeline__day">1</span>
+          <span class="timeline__month">Avril</span>
+        </div>
+        <div class="timeline__post">
+          <div class="timeline__content">
+            <p><b>Stage dans un laboratoire de recherche de 6 mois au City College de New York (Matlab): </b>
+            <li>Développement d\'algorithmes de routage de véhicules à l\'aide de l\'échantillonage par copules.</li>
+            <li>Analyse des comportements de mobilité des personnes âgées et handicapées.</li>
+            <li>Modélisation d’un service de transport à la demande avec partage de courses dans la ville de New York. </li>
+            <li>Dimensionnement de la flotte necessaire au fonctionnement du service.</li></p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="timeline__group">
+      <span class="timeline__year">2016</span>
+      <div class="timeline__box">
+        <div class="timeline__date">
+          <span class="timeline__day">28</span>
+          <span class="timeline__month">Aug</span>
+        </div>
+        <div class="timeline__post">
+          <div class="timeline__content">
+            <p>Upon moving to Brooklyn that summer, I began photographing weddings in Chicago</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>'
