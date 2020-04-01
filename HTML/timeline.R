bce358d1bf7ae4417fcf37325b00c3359b14cd36
mzellou <marouane.zellou@developpement-durable.gouv.fr>
2020-04-01 17:42:00 +0200

    Ajout des langues Correction de la timeviz (durée)

--- HTML/timeline.R
+++ HTML/timeline.R
@@ -1,5 +1,6 @@
 '<div class="page">
   <div class="timeline">
+  
     <div class="timeline__group">
       <span class="timeline__year">2014</span>
       <div class="timeline__box">
@@ -14,6 +15,7 @@
         </div>
       </div>
     </div>
+    
     <div class="timeline__group">
       <span class="timeline__year">2016</span>
       <div class="timeline__box">
@@ -23,11 +25,11 @@
         </div>
         <div class="timeline__post">
           <div class="timeline__content">
-            <p><b>Projet spécifique de recherche au sein du Laboratoire d\'Ingéniérie Circulation Transport (LICIT): Lyon, France (Java): </b>
+            <p><b>Projet spécifique de recherche au sein du Laboratoire d\'Ingéniérie Circulation Transport (LICIT) à Lyon (Java): </b>
             <li>Travail au sein d’une équipe de recherche dans le cadre du projet MOCoPo dirigé par le Dr Christine BUISSON.</li>
             <li>Mise a jour du site internet du projet (Typo3).</li>
             <li>Mise à jour du programme Java de traitement d\'images : Ce programme permet l\'analyse des conditions de circulation à partir de photos
-            aériennes. Pour plus d\'informations : <a href="https://mocopo.ifsttar.fr/">visitez le site de MOCoPo</a>  </li>
+            aériennes. Pour plus d\'informations : <a href="https://mocopo.ifsttar.fr/">visitez le site de MOCoPo</a>  </li></p>
           </div>
         </div>
       </div>
@@ -47,19 +49,57 @@
         </div>
       </div>
     </div>
+    
     <div class="timeline__group">
-      <span class="timeline__year">2016</span>
+      <span class="timeline__year">2017</span>
       <div class="timeline__box">
         <div class="timeline__date">
-          <span class="timeline__day">28</span>
-          <span class="timeline__month">Aug</span>
+          <span class="timeline__day">1</span>
+          <span class="timeline__month">Sept</span>
         </div>
         <div class="timeline__post">
           <div class="timeline__content">
-            <p>Upon moving to Brooklyn that summer, I began photographing weddings in Chicago</p>
+            <p><b>Ensemble des cours suivis avec Python </b></p>
+          </div>
+        </div>
+      </div>
+      <div class="timeline__box">
+        <div class="timeline__date">
+          <span class="timeline__day">1</span>
+          <span class="timeline__month">Avril</span>
+        </div>
+        <div class="timeline__post">
+          <div class="timeline__content">
+            <p><b>Stage de fin d\'études au sein du Laboratoire d\'Ingéniérie Circulation Transport (LICIT) à Lyon (Matlab): </b>
+            <li>blabla</li>
+            <li>blabla</li>
+            <li>blabla </li>
+            <li>blabla</li></p>
           </div>
         </div>
       </div>
     </div>
+    
+    <div class="timeline__group">
+      <span class="timeline__year">2018</span>
+      <div class="timeline__box">
+        <div class="timeline__date">
+          <span class="timeline__day">1</span>
+          <span class="timeline__month">Oct</span>
+        </div>
+        <div class="timeline__post">
+          <div class="timeline__content">
+            <p><b>Chef de projet informatique au Ministère de la Transition Écologique et Solidaire </b>
+            <li>ZeroRS et CoQua</li>
+            <li>Conversion des programmes SAS en R</li>
+            <li>Réécriture des programmes de redressements : hot-deck / Naive Bayes / Polynomique ordinal </li>
+            <li>Mise en place du contrôle de version de l\'ensemble des programmes (Github)</li>
+            <li>Dockerisation des applications pour déploiement en interne</li>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    
   </div>
 </div>'
